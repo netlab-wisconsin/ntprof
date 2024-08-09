92fdb28c9decb8dfe8f7bc37b0434f872da6dc45
yuyuan <yuyuan@node1.ntprof2.x-nova-pg0.wisc.cloudlab.us>
2024-08-09 21:22:54 +0000

    target: iperf_client

--- monitor/nvmet_tcp_monitor/user/u_nttm.c
+++ monitor/nvmet_tcp_monitor/user/u_nttm.c
@@ -285,6 +285,12 @@ int main(int argc, char **argv) {
   init_tcp_layer_monitor();
 
   while (keep_running) {
+    // //  printf("\033[H\033[J");
+    // printf("Arguments:\n");
+    // print_args(args);
+    // // print_blk_layer_stat();
+    // print_nvmet_tcp_layer_stat();
+    // print_tcp_layer_stat();
     sleep(1);
   }
 
