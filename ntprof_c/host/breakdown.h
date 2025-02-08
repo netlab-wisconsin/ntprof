07299d254fb34558bb8683ed7d74d9ac65365f1e
yuyuan <yuyuan@node0.ntprof.x-nova-pg0.wisc.cloudlab.us>
2025-02-08 11:17:23 -0600

    breakdown union

--- ntprof_c/host/breakdown.h
+++ ntprof_c/host/breakdown.h
@@ -1,32 +1,10 @@
 #ifndef BREAKDOWN_H
 #define BREAKDOWN_H
 
+
+#include "../include/analyze.h"
 #include "../include/statistics.h"
 
-#define READ_BREAKDOWN_FIELDS(X)                 \
-    X(blk_submission_queueing)                   \
-    X(nvme_tcp_submission_queueing)              \
-    X(nvme_tcp_request_processing)               \
-    X(nvme_tcp_waiting)                          \
-    X(nvmet_tcp_cmd_capsule_queueing)            \
-    X(nvmet_tcp_cmd_processing)                  \
-    X(nvmet_tcp_submit_to_blk)                   \
-    X(nvmet_tcp_completion_queueing)             \
-    X(nvmet_tcp_response_processing)             \
-    X(nvme_tcp_completion_queueing)              \
-    X(nvme_tcp_response_processing)              \
-    X(blk_completion_queueing)                   \
-    X(blk_e2e)                                   \
-    X(nvme_tcp_e2e)                              \
-    X(nvmet_tcp_e2e)                             \
-    X(networking)
-
-struct read_breakdown {
-    u64 cnt;
-#define X(field) u64 field;
-    READ_BREAKDOWN_FIELDS(X)
-    #undef X
-};
 
 static inline void init_read_breakdown(struct read_breakdown *b) {
     memset(b, 0, sizeof(*b));
@@ -47,30 +25,6 @@ pr_info(#field ": %lu\n", (unsigned long)((b->field / b->cnt) / 1000));
 #undef X
 }
 
-#define WRITE_BREAKDOWN_S_FIELDS(X)               \
-    X(blk_submission_queueing)                    \
-    X(nvme_tcp_submission_queueing)               \
-    X(nvme_tcp_request_processing)                \
-    X(nvmet_tcp_cmd_capsule_queueing)             \
-    X(nvmet_tcp_cmd_processing)                   \
-    X(nvmet_tcp_submit_to_blk)                    \
-    X(nvmet_tcp_completion_queueing)              \
-    X(nvmet_tcp_response_processing)              \
-    X(nvme_tcp_waiting_for_resp)                  \
-    X(nvme_tcp_completion_queueing)               \
-    X(blk_completion_queueing)                    \
-    X(blk_e2e)                                    \
-    X(nvme_tcp_e2e)                               \
-    X(nvmet_tcp_e2e)                              \
-    X(networking)
-
-struct write_breakdown_s {
-    u64 cnt;
-
-#define X(field) u64 field;
-    WRITE_BREAKDOWN_S_FIELDS(X)
-#undef X
-};
 
 static inline void init_write_breakdown_s(struct write_breakdown_s *b)
 {
@@ -92,38 +46,7 @@ pr_info(#field ": %lu\n", (unsigned long)((b->field / b->cnt) / 1000));
 #undef X
 }
 
-#define WRITE_BREAKDOWN_L_FIELDS(X)               \
-    X(blk_submission_queueing)                    \
-    X(nvme_tcp_cmd_submission_queueing)           \
-    X(nvme_tcp_cmd_capsule_processing)            \
-    X(nvmet_tcp_cmd_capsule_queueing)             \
-    X(nvmet_tcp_cmd_processing)                   \
-    X(nvmet_tcp_r2t_submission_queueing)          \
-    X(nvmet_tcp_r2t_resp_processing)              \
-    X(nvmet_tcp_wait_for_data)                    \
-    X(nvme_tcp_wait_for_r2t)                      \
-    X(nvme_tcp_r2t_resp_queueing)                 \
-    X(nvme_tcp_data_pdu_submission_queueing)      \
-    X(nvme_tcp_data_pdu_processing)               \
-    X(nvmet_tcp_write_cmd_queueing)               \
-    X(nvmet_tcp_write_cmd_processing)             \
-    X(nvmet_tcp_submit_to_blk)                    \
-    X(nvmet_tcp_completion_queueing)              \
-    X(nvmet_tcp_response_processing)              \
-    X(nvme_tcp_wait_for_resp)                     \
-    X(nvme_tcp_resp_queueing)                     \
-    X(blk_completion_queueing)                    \
-    X(blk_e2e)                                    \
-    X(networking)
 
-
-struct write_breakdown_l {
-    u64 cnt;
-
-#define X(field) u64 field;
-    WRITE_BREAKDOWN_L_FIELDS(X)
-#undef X
-};
 
 /* 3. 初始化 */
 static inline void init_write_breakdown_l(struct write_breakdown_l *b)
--- ntprof_c/include/analyze.h
+++ ntprof_c/include/analyze.h
@@ -12,4 +12,103 @@ struct analyze_arg {
     struct profile_result result;
 };
 
+#define READ_BREAKDOWN_FIELDS(X)                 \
+X(blk_submission_queueing)                   \
+X(nvme_tcp_submission_queueing)              \
+X(nvme_tcp_request_processing)               \
+X(nvme_tcp_waiting)                          \
+X(nvmet_tcp_cmd_capsule_queueing)            \
+X(nvmet_tcp_cmd_processing)                  \
+X(nvmet_tcp_submit_to_blk)                   \
+X(nvmet_tcp_completion_queueing)             \
+X(nvmet_tcp_response_processing)             \
+X(nvme_tcp_completion_queueing)              \
+X(nvme_tcp_response_processing)              \
+X(blk_completion_queueing)                   \
+X(blk_e2e)                                   \
+X(nvme_tcp_e2e)                              \
+X(nvmet_tcp_e2e)                             \
+X(networking)
+
+struct read_breakdown {
+    unsigned long long cnt;
+#define X(field) unsigned long long field;
+    READ_BREAKDOWN_FIELDS(X)
+    #undef X
+};
+
+
+#define WRITE_BREAKDOWN_S_FIELDS(X)               \
+X(blk_submission_queueing)                    \
+X(nvme_tcp_submission_queueing)               \
+X(nvme_tcp_request_processing)                \
+X(nvmet_tcp_cmd_capsule_queueing)             \
+X(nvmet_tcp_cmd_processing)                   \
+X(nvmet_tcp_submit_to_blk)                    \
+X(nvmet_tcp_completion_queueing)              \
+X(nvmet_tcp_response_processing)              \
+X(nvme_tcp_waiting_for_resp)                  \
+X(nvme_tcp_completion_queueing)               \
+X(blk_completion_queueing)                    \
+X(blk_e2e)                                    \
+X(nvme_tcp_e2e)                               \
+X(nvmet_tcp_e2e)                              \
+X(networking)
+
+struct write_breakdown_s {
+    unsigned long long cnt;
+
+#define X(field) unsigned long long field;
+    WRITE_BREAKDOWN_S_FIELDS(X)
+#undef X
+};
+
+#define WRITE_BREAKDOWN_L_FIELDS(X)               \
+X(blk_submission_queueing)                    \
+X(nvme_tcp_cmd_submission_queueing)           \
+X(nvme_tcp_cmd_capsule_processing)            \
+X(nvmet_tcp_cmd_capsule_queueing)             \
+X(nvmet_tcp_cmd_processing)                   \
+X(nvmet_tcp_r2t_submission_queueing)          \
+X(nvmet_tcp_r2t_resp_processing)              \
+X(nvmet_tcp_wait_for_data)                    \
+X(nvme_tcp_wait_for_r2t)                      \
+X(nvme_tcp_r2t_resp_queueing)                 \
+X(nvme_tcp_data_pdu_submission_queueing)      \
+X(nvme_tcp_data_pdu_processing)               \
+X(nvmet_tcp_write_cmd_queueing)               \
+X(nvmet_tcp_write_cmd_processing)             \
+X(nvmet_tcp_submit_to_blk)                    \
+X(nvmet_tcp_completion_queueing)              \
+X(nvmet_tcp_response_processing)              \
+X(nvme_tcp_wait_for_resp)                     \
+X(nvme_tcp_resp_queueing)                     \
+X(blk_completion_queueing)                    \
+X(blk_e2e)                                    \
+X(networking)
+
+
+struct write_breakdown_l {
+    unsigned long long cnt;
+
+#define X(field) unsigned long long field;
+    WRITE_BREAKDOWN_L_FIELDS(X)
+#undef X
+};
+
+enum EBreakdownType {
+    BREAKDOWN_READ,
+    BREAKDOWN_WRITES,
+    BREAKDOWN_WRITEL
+};
+
+struct breakdown {
+    enum EBreakdownType type;
+    union {
+        struct read_breakdown read;
+        struct write_breakdown_s writes;
+        struct write_breakdown_l writel;
+    };
+};
+
 #endif //ANALYZE_H
